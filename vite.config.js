13abfa44d5600b3c25d83e0fdadc8c48da0a5dca
AndreySakhnenko <senya310594@gmail.com>
2024-04-18 14:27:23 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  publicPath: "/Image-Carousel/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
