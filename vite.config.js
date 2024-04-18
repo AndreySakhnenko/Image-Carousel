cb94373b0ece33b63e593c2bc4cd8666884290f4
AndreySakhnenko <senya310594@gmail.com>
2024-04-18 21:13:20 +0300

    test pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  transpileDependencies: true,
   base: process.env.NODE_ENV === 'production' ? '/Image-Carousel/' : '/',
   plugins: [
     vue(),
