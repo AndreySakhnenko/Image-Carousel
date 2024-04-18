322120093197d83eb0003025abf691ff1ee3b494
AndreySakhnenko <senya310594@gmail.com>
2024-04-19 00:58:17 +0300

    test

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../pages/HomePage.vue'
 
 const router = createRouter({
-  history: createWebHistory('/Image-Carousel/'),
+  history: createWebHistory('/Image-Carousel'),
   routes: [
     {
       path: '/',
