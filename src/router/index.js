861135552c7afa9e3b069da1dad0917452ba59fd
AndreySakhnenko <senya310594@gmail.com>
2024-04-18 15:01:29 +0300

    12

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../pages/HomePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  base: "/Image-Carousel/",
   routes: [
     {
       path: '/',
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Image-Carousel/",
+  base: process.env.NODE_ENV === 'production' ? '/Image-Carousel/' : '/',
   plugins: [
     vue(),
   ],
