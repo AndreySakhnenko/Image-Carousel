d1a863fa999f174a8bb5c6469db96a3a088ce477
AndreySakhnenko <senya310594@gmail.com>
2024-04-19 09:55:28 +0300

    fexed err

--- src/components/SelectedImages.vue
+++ src/components/SelectedImages.vue
@@ -1,17 +1,20 @@
 <template>
-  <div>
+  <div class="selected">
     <h3>Обрані зображення:</h3>
     <ul>
-      <li v-for="selectedImage in selectedImages" :key="selectedImage.id">
+      <li class="selected_item" v-for="selectedImage in selectedImages" :key="selectedImage.id">
         <span>{{ selectedImage.author }}</span>
         <a :href="selectedImage.url" target="_blank">{{ selectedImage.url }}</a>
+        <button class="selected_btn" @click="removeFromSelectedImages(selectedImage)">
+          <img src="../assets/delete.svg" alt="delete">   
+        </button>
       </li>
     </ul>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { defineProps } from "vue";
 
 const props = defineProps({
   selectedImages: {
@@ -19,5 +22,26 @@ const props = defineProps({
     required: true,
   },
 });
+
+const removeFromSelectedImages = (image) => {
+  const index = props.selectedImages.findIndex((img) => img.id === image.id);
+  if (index !== -1) {
+    props.selectedImages.splice(index, 1);
+  }
+};
 </script>
-<style scoped></style>
+
+<style scoped>
+.selected{
+  padding: 0 15px;
+}
+.selected_item{
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+.selected_btn{
+  border: none;
+  background: transparent;
+}
+</style>
